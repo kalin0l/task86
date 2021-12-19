3eabbd353c2912b7a62216bdd59e7e38285c093b
kalin0l <aleksiev2494@gmail.com>
2021-12-19 11:09:02 +0200

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,21 @@
 import "../scss/app.scss";
-const R = require("ramda");
+// const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-  const ul = document.querySelector("ul");
+  
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  // const isEven = (n) => n % 2 === 0;
+  // const filteredArray = R.filter(isEven, numbersArray);
+  const nativeJS = numbersArray.filter(item => item % 2 === 0);
+  console.log(nativeJS[0],nativeJS[1]);
+  const ul = document.querySelector("ul");
+  const li1 = document.createElement("li");
+  const li2 = document.createElement("li");
+  const li1Content = document.createTextNode(`${nativeJS[0]}`);
+  const li2Content = document.createTextNode(`${nativeJS[1]}`);
+
+  li1.appendChild(li1Content);
+  li2.appendChild(li2Content);
+  ul.appendChild(li1);
+  ul.appendChild(li2);
 });
